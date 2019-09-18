efbedebfb5aa328c9b663d9897a97a3be753d97e
deepashammi <54535695+deepashammi@users.noreply.github.com>
2019-09-18 11:26:30 +0530

    Add files via upload

---
+++ Script1.js
@@ -0,0 +1,14 @@
+// JavaScript source code
+<html>
+    <head></head>
+    <form name="calculator">
+        <table>
+            <tr>
+                <td>
+                    <input type="text" name="display" id="display" disabled>
+                </td>
+            </tr>
+                <tr>
+                    <td><input type="button" name="one" value="1"> onclick="calculator.display.value +='1'"></td>
+                    <td><input type="button">               
+                    
